955b4886ccf2a20b836cddcdaad721ed892a1cbe
NBRET-TOUCH-WASH <nbret.touchard.nsi@protonmail.com>
2021-10-25 21:38:06 +0200

    added ascii map test-file

---
+++ source/test/map/map.py
@@ -0,0 +1,39 @@
+"""
+notes:
+    ? docs:
+        ? I said pick up the can.
+
+    § TODO:
+        § Amplify weapons on Wallhammer.
+
+    % FIXME:
+        % Target compromised: move in, move in.
+
+        & FIX:
+            & Overwatch, target one sterilized.
+
+    µ WHYNOT:
+        µ Overwatch reports possible hostiles inbound.
+
+    ! IMPORTANT:
+        ! Roger that. Waiting for contact. Over.
+
+    $ LOGS:
+        $ Ready weapons, stay alert.
+"""
+
+
+#encoding
+#coding:utf-8
+
+
+#libraries/modules
+
+
+#classes
+
+
+#functions
+
+
+#script
