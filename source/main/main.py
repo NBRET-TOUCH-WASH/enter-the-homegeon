125fd2caa0c856d67910e1048a22e865b41683e5
NBRET-TOUCH-WASH <nbret.touchard.nsi@protonmail.com>
2021-10-28 00:38:39 +0200

    created finalized code folder
    - added 'main' folder
    - created 'main.py'

---
+++ source/main/main.py
@@ -0,0 +1,42 @@
+"""
+notes:
+    ? docs:
+        ? I said pick up the can.
+
+    § TODO:
+        § Amplify weapons on Wallhammer.
+
+    % FIXME:
+        % Target compromised: move in, move in.
+
+        & FIX:
+            & Overwatch, target one sterilized.
+
+    µ WHYNOT:
+        µ Overwatch reports possible hostiles inbound.
+
+    ! IMPORTANT:
+        ! Roger that. Waiting for contact. Over.
+
+    $ LOGS:
+        $ Ready weapons, stay alert.
+"""
+
+
+#encoding
+#coding:utf-8
+
+
+#libraries/modules
+
+
+#libs setup
+
+
+#classes
+
+
+#functions
+
+
+#script
