e6ba81a4b601780083249dcbbe6fefb3d341ea77
NBRET-TOUCH-WASH <nbret.touchard.nsi@protonmail.com>
2021-11-04 00:43:16 +0100

    tried adding sound
    sound won't work fuck it lmao

---
+++ source/main/libs/audio/sound.py
@@ -0,0 +1,50 @@
+"""
+notes:
+    ? docs:
+        ? I said pick up the can.
+
+    § TODO:
+        § Amplify weapons on Wallhammer.
+
+    % FIXME:
+        % THE WHOLE FUCKING THING LOL
+
+        & FIX:
+            & IDK DEAL WITH IT CUZ I WONT
+
+    µ WHYNOT:
+        µ Overwatch reports possible hostiles inbound.
+
+    ! IMPORTANT:
+        ! Roger that. Waiting for contact. Over.
+
+    $ LOGS: (d/m/y)
+        $ 04/11/2021
+            $ 00:40AM
+                $ fuck this shit 🖕🖕🖕
+                $ i can't make this work
+                $ i tried with so many different libraries
+                $ playsound ; pygame ; pydub...
+                $ nothing works
+                $ screw it
+                $ there won't be sound in this project idc
+"""
+
+
+#encoding
+#coding:utf-8
+
+
+#libraries/modules
+
+
+#libs setup
+
+
+#classes
+
+
+#functions
+
+
+#script
